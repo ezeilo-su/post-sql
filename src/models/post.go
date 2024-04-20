28367511e94d59c84794261adfc60b7108f40f5d
Sunday Ezeilo <ezeilo.su@gmail.com>
2024-04-20 10:51:05 +0100

    Separate concerns

--- src/models/post.go
+++ src/models/post.go
@@ -4,11 +4,11 @@ import "time"
 
 // PostModel is the data model for a blog post
 type Post struct {
-	UID       string    `db:"id" json:"uid"`
-	User      string    `db:"user" json:"user"`
-	Title     string    `db:"title" json:"title"`
-	Content   string    `db:"content" json:"content"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
-	Image     string    `db:"image,omitempty" json:"image,omitempty"`
+	UID       string    `json:"uid"`
+	User      string    `json:"user"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Image     string    `json:"image,omitempty"`
 }
